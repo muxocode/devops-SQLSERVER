117a6c1dbdfbcff92ceab2217612fa58fd72b2ea
Miguel Angel del Campo Morales <miguel.campo@altran.com>
2019-04-26 13:56:05 +0200

    Corrección BUG

--- scripts/setEnv.js
+++ scripts/setEnv.js
@@ -3,16 +3,16 @@ require('dotenv').config();
 const args = require('yargs').argv;
 const fs = require('fs')
 
-//mkdir if nor exists
-if (!fs.existsSync(process.env.HOME)){
-    fs.mkdirSync(process.env.HOME);
-}
-
 //SET .env
 const mydata = path.resolve("./");
 process.env.HOME = (args.home || mydata) + (args.folder || "/mydata")
 process.env.PORT = args.port || "1432";
 process.env.PASSWORD = args.password || "deVops.Docker!";
+
+//mkdir if nor exists
+if (!fs.existsSync(process.env.HOME)){
+    fs.mkdirSync(process.env.HOME);
+}
 
 //Save .env
 fs.writeFileSync('./.env', "HOME="+process.env.HOME);
